25fda7633db39c17d209610a17aeb43f48dd3b3b
Your Name <you@example.com>
2020-07-10 21:04:14 +0545

    Project

--- js/script.js
+++ js/script.js
@@ -17,3 +17,19 @@ function outsoln()
 {
 	document.querySelector('#support').style.display = "none";
 }
+
+function showsidebar()
+{
+	document.querySelector(".sidebar").style.flex = "1";
+	document.querySelector(".sidebar").style.width = "230px";
+	document.querySelector(".sidebar").style.transition = "0.1s";
+	document.querySelector("#ham-menu").style.display = "none";
+
+}
+
+function hidesidebar()
+{  
+	document.querySelector(".sidebar").style.width = "0px";
+	document.querySelector(".sidebar").style.flex = "0";
+	document.querySelector("#ham-menu").style.display = "inline";
+}
